43f82a1f61ade5d276d99615f98dcfc29b45d0de
Hafiz1st <hafiz100st@gmail.com>
2020-04-26 18:22:48 +0800

    penambahan class PenerapanLoop

---
+++ src/PenerapanLoop.java
@@ -0,0 +1,11 @@
+public class PenerapanLoop {
+    public static void main(String[] args) {
+        System.out.println("Bom akan meledak pada: ");
+
+        for (int i = 5; i > 0; i--) {
+            System.out.println( i + " Detik.");
+        }
+
+        System.out.println("BOOM!! Bom meledak.");
+    }
+}
